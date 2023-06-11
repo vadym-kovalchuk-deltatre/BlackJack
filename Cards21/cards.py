bdf93aa220ad1a0b3d11e85c5f51ff954631690e
Vadym Kovalchuk <106161570+vadym-kovalchuk-deltatre@users.noreply.github.com>
2023-06-11 13:09:06 +0300

    Added suit

--- Cards21/cards.py
+++ Cards21/cards.py
@@ -1,14 +1,16 @@
-from random import randint
-cards:list[int] = [index for index in range(2,12)]
+import random
+cards:list[int] = [11]+[index for index in range(2,11)]
 cards += [10]*4
 cardsHearts:list[int] = cards.copy()
 cardsDiamonds:list[int] = cards.copy()
 cardsSpades:list[int] = cards.copy()
 cardsClubs:list[int] = cards.copy()
-# 0-hearts, 1-diamonds, 2-spades, 3-clubs
-suits:list[list[int]] = [cardsHearts,cardsDiamonds,cardsSpades,cardsClubs]
+suits:tuple[str] = ("Hearts","Diamonds","Spades","Clubs")
+suitsCards:list[list[int]] = [cardsHearts,cardsDiamonds,cardsSpades,cardsClubs]
 
-def getCard()->int:
-	suit = suits[randint(0, len(suits) - 1)]
-	card = suit.pop(randint(0, len(suit) -1))
-	return card
+def getCard()->tuple[str,int]:
+	suitIndex = random.randrange(0, len(suits))
+	suitCards = suitsCards[suitIndex]
+	card = suitCards.pop(random.randint(0, len(suitCards) - 1))
+
+	return (suits[suitIndex],card)
