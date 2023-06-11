fa82452da6d5982bf07047687bf95d08fb5e1ce1
Vadym Kovalchuk <106161570+vadym-kovalchuk-deltatre@users.noreply.github.com>
2023-06-11 14:05:42 +0300

    Addied docs

--- Cards21/cards.py
+++ Cards21/cards.py
@@ -1,4 +1,5 @@
 import random
+
 cards:list[int] = [11]+[index for index in range(2,11)]
 cards += [10]*4
 cardsHearts:list[int] = cards.copy()
@@ -8,9 +9,15 @@ cardsClubs:list[int] = cards.copy()
 suits:tuple[str] = ("Hearts","Diamonds","Spades","Clubs")
 suitsCards:list[list[int]] = [cardsHearts,cardsDiamonds,cardsSpades,cardsClubs]
 
-def getCard()->tuple[str,int]:
+def getCard()->list[str,int]:
+	"""
+	The function returns a randomly selected card from a deck of cards, along with its corresponding
+	suit.
+	:return: A list containing a string representing the suit of the card and an integer representing
+	the value of the card.
+	"""
 	suitIndex = random.randrange(0, len(suits))
 	suitCards = suitsCards[suitIndex]
 	card = suitCards.pop(random.randint(0, len(suitCards) - 1))
 
-	return (suits[suitIndex],card)
+	return [suits[suitIndex],card]
