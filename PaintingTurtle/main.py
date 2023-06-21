ca6909b13c031e397a08da23b44a2a6446894bac
Vadym Kovalchuk <106161570+vadym-kovalchuk-deltatre@users.noreply.github.com>
2023-06-21 20:46:41 +0300

    Add keys

--- PaintingTurtle/main.py
+++ PaintingTurtle/main.py
@@ -1,6 +1,8 @@
 from turtle import Turtle, Screen, colormode
 import random
 
+STEP: int = 10
+
 
 def _random_color() -> int:
     return random.randint(0, 250)
@@ -13,7 +15,6 @@ def get_color() -> tuple[int, int, int]:
 def set_directions() -> list[int, int, int, int]:
     dirs = []
     for direction in range(0, 450, 90):
-        print(direction)
         dirs.append(direction)
     return dirs
 
@@ -27,9 +28,28 @@ def get_direction(dirs: list[int, int, int, int]) -> int:
     return random.choice(dirs)
 
 
-def on_click() -> bool:
-    print("Click")
-    return False
+def on_right() -> None:
+    timmy.right(90)
+
+
+def on_left() -> None:
+    timmy.left(90)
+
+
+def on_up() -> None:
+    timmy.fd(STEP)
+
+
+def on_back() -> None:
+    timmy.fd(-STEP)
+
+
+def on_exit_screen() -> None:
+    screen.bye()
+
+
+def on_change_dir() -> None:
+    timmy.left(get_direction(directions))
 
 
 def check_margins() -> bool:
@@ -43,12 +63,21 @@ timmy: Turtle = Turtle()
 timmy.shape("circle")
 timmy.resizemode("user")
 timmy.shapesize(.1, .1, .2)
+
 colormode(255)
-screen = Screen()
+screen: Screen = Screen()
 screen.title("Timmy Random Move")
+
+screen.listen()
+screen.onkey(on_exit_screen, 'x')
+screen.onkey(on_change_dir, 'space')
+screen.onkey(on_up, "Up")
+screen.onkey(on_left, "Left")
+screen.onkey(on_right, "Right")
+screen.onkey(on_back, "Down")
+
 while True:
     timmy.pencolor(get_color())
     timmy.speed(random.randint(3, 10))
-    timmy.fd(10)
+    timmy.fd(STEP)
     timmy.left(get_direction(directions))
-
