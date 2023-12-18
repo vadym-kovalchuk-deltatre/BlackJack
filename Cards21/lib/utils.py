b5c99bdd66dcd6f53b1058c807266e1e1df1a02b
TeeZ0NE <106161570+vadym-kovalchuk-deltatre@users.noreply.github.com>
2023-12-18 21:06:00 +0200

    * moved to separate folder * used decorators * upd card's suits class

--- Cards21/lib/utils.py
+++
@@ -1,9 +0,0 @@
-def print_underline(count: int, symbol: str = "=") -> None:
-    """
-    The print_underline function prints a line of characters to the console.
-
-    :param count: int: Specify the number of times to print the symbol
-    :param symbol: str: Specify the character that will be used to print the underline
-    :return: None
-    :doc-author: Trelent"""
-    print(symbol * count)
--- temp.py
+++
@@ -1,4 +0,0 @@
-lister = [["a", 1], ["b", 2]]
-print(lister)
-newLister = sum(list(map(lambda x: x[1], lister)))
-print(newLister)
